df71790704bf6cf6f43659a1751ec75cbfa3986f
Cynthia Zhou <lbx01072004@gmail.com>
2020-05-21 23:12:28 +1000

    add

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, devour) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Beef Burger', true);
